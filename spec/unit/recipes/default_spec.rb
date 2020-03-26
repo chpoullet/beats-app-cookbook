7f5b382ce5375daaea7e9d3d69b954b508a04a0c
chpoullet <59974805+chpoullet@users.noreply.github.com>
2020-03-26 14:49:07 +0000

    Update default_spec.rb

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -17,13 +17,16 @@ describe 'beatsapp_cookbook::default' do
     end
   end
 
-  context 'When all attributes are default, on CentOS 7' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'centos', '7'
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'should update source list' do
+      expect(chef_run).to update_apt_update('update_sources')
     end
-  end
+    it 'apt-transport-https' do
+      expect(chef_run).to install_package "apt-transport-https"
+    end
+    it 'metricbeat' do
+     expect(chef_run).to install_package "metricbeat"
+    end
+    it 'filebeat' do
+     expect(chef_run).to install_package "filebeat"
+    end
 end
